d99da7b84513580b545ad61a8a469d2d56b0b132
includeCpp <anton.a.v.1987@gmail.com>
2022-05-22 07:06:45 +0300

    shake_sort, hair_sort, insertion_sort, selection_sort, double_selection_sort added

---
+++ data_structures.cpp
@@ -0,0 +1,111 @@
+#ifndef __data__structures__
+#define __data__structures__
+
+#include<algorithm>
+#include<iostream>
+
+namespace data_structures{
+	template<typename T>
+	void bsort(T& array){   				
+		for(int i = 0; i < array.size(); ++i){
+			for(int j = 0; j < array.size() - 1; ++j){
+				if(array[j] > array[j + 1]){
+					std::swap(array[j], array[j + 1]);
+				}
+			}
+		}
+	}
+
+	template<typename T>
+	void shake_sort(T& array){
+		int leftMark = 0;
+		int rightMark = array.size() - 1;
+		while(leftMark <= rightMark){
+			for(int i = leftMark; i <= rightMark; ++i){
+				if(array[i] > array[i + 1]){
+					std::swap(array[i], array[i + 1]);
+				}
+			}
+			rightMark--;
+
+			for(int i = rightMark; i >= leftMark; --i){
+				if(array[i] < array[i - 1]){
+					std::swap(array[i], array[i - 1]);
+				}
+			}
+			leftMark++;
+		}
+	}
+
+	template<typename T>
+	void hair_sort(T& array){
+		int tmp, k;
+		int n = array.size() - 1;
+		int s = n;
+		while(n > 1){
+			s /= 1.247f;
+			if(s < 1) s = 1;
+			k = 0;
+			for(int i = 0; i + s <= n; ++i){
+				if(array[i] > array[i + s]){
+					tmp = array[i];
+					array[i] = array[i + s];
+					array[i + s] = tmp;
+					k = i;
+				}
+			}
+			if(s == 1) n = k + 1;
+		}
+	}
+
+	template<typename T>
+	void insertion_sort(T& array){
+		for(int i = 1; i < array.size(); ++i){
+			for(int j = i; j > 0 && array[j - 1] > array[j]; j--){
+				std::swap(array[j - 1], array[j]);
+			}
+		}
+	}
+
+	template<typename T>
+	void selection_sort(T& array){
+		int min = 0;
+		int max = array.size();
+		while(min < max){
+			//std::cout << max << std::endl;
+			for(int i = min + 1; i < max; i++){
+				if(array[i] < array[min]){
+					std::swap(array[i], array[min]);
+				}
+			}
+			min++;
+		}
+	}
+
+	template<typename T>
+	void double_selection_sort(T& array){
+		int min = 0;
+		int max = array.size();
+		while(min < max){
+			//std::cout << max << std::endl;
+			for(int i = min + 1; i < max; i++){
+				if(array[i] < array[min]){
+					std::swap(array[i], array[min]);
+				}
+			}
+			for(int i = max - 2; i > min; i--){
+				if(array[i] > array[max - 1]){
+					std::swap(array[i], array[max - 1]);
+				}
+			}
+			min++;
+			max--;
+		}
+	}
+
+
+
+} // data_structures
+
+#endif
+
