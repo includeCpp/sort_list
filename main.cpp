0c05bff67b316743fd03547176c0af89e30ed4ea
includeCpp <anton.a.v.1987@gmail.com>
2022-08-30 03:05:37 +0300

    cmp structure added

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
-#include "data_structures.cpp"
+#include "sorting_algorithms.hpp"
 
 #include <iostream>
 #include <algorithm>
 #include <vector>
 
+struct cmp{
+	template<typename T, typename Q>
+		bool operator()(T lhs, Q rhs){
+			return lhs > rhs;
+		}
+};
+
+struct cmp_2{
+		template<typename T, typename Q>
+		bool operator()(T lhs, Q rhs){
+			return lhs < rhs;
+		}
+};
+
 int main(){
 	//list of sorts
-	//std::cout << "\nBubble sort" << std::endl; 
+	std::cout << "\nBubble sort" << std::endl; 
 	std::vector<int> my_vec = {3, 5, 1, 2, 0, 4, 9, 6, 8, 7};
-	data_structures::bsort(my_vec);
+	sorting_algorithms::bsort<std::vector<int>, cmp>(my_vec);
 	for(int i = 0; i < my_vec.size(); ++i){
 		std::cout << my_vec[i] << " ";
 	}
@@ -16,15 +30,15 @@ int main(){
 	std::cout << std::endl;
 	std::cout << "Shaker sort" << std::endl;
 	my_vec = {3, 5, 1, 2, 0, 4, 9, 6, 8, 7};
-	data_structures::shake_sort(my_vec);
+	sorting_algorithms::shake_sort<std::vector<int>, cmp>(my_vec);
 	for(int i = 0; i < my_vec.size(); ++i){
 		std::cout << my_vec[i] << " ";
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
-	std::cout << "Hair sort" << std::endl;
+	std::cout << "Comb sort" << std::endl;
 	my_vec = {3, 5, 1, 2, 0, 4, 9, 6, 8, 7};
-	data_structures::hair_sort(my_vec);
+	sorting_algorithms::comb_sort<std::vector<int>, cmp>(my_vec);
 	for(int i = 0; i < my_vec.size(); ++i){
 		std::cout << my_vec[i] << " ";
 	}
@@ -32,7 +46,7 @@ int main(){
 	std::cout << std::endl;
 	std::cout << "Insertion sort" << std::endl;
 	my_vec = {3, 5, 1, 2, 0, 4, 9, 6, 8, 7};
-	data_structures::insertion_sort(my_vec);
+	sorting_algorithms::insertion_sort<std::vector<int>, cmp>(my_vec);
 	for(int i = 0; i < my_vec.size(); ++i){
 		std::cout << my_vec[i] << " ";
 	}
@@ -40,7 +54,7 @@ int main(){
 	std::cout << std::endl;
 	std::cout << "Selection sort" << std::endl;
 	my_vec = {3, 5, 1, 2, 0, 4, 9, 6, 8, 7};
-	data_structures::selection_sort(my_vec);
+	sorting_algorithms::selection_sort<std::vector<int>, cmp>(my_vec);
 	for(int i = 0; i < my_vec.size(); ++i){
 		std::cout << my_vec[i] << " ";
 	}
@@ -48,15 +62,15 @@ int main(){
 	std::cout << std::endl;
 	std::cout << "Double selection sort" << std::endl;
 	my_vec = {3, 5, 32, 1, 10, 2, 50, 0, 4, 9, 13, 6, 101, 322, 19 ,8, 7};
-	data_structures::double_selection_sort(my_vec);
+	sorting_algorithms::double_selection_sort<std::vector<int>, cmp>(my_vec);
 	for(int i = 0; i < my_vec.size(); ++i){
 		std::cout << my_vec[i] << " ";
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
 	std::cout << "Quick sort" << std::endl;
-	my_vec = {9, 12, 10, 8, 11, 3, 6, 4, 2, 5, 1, 7};
-	data_structures::qsort(0, 12, my_vec);
+	my_vec = {9, 12, 10, 8, 11, 3, 6, 4, 2, 5, 1, 7, 8, 89, 45, 55};
+	sorting_algorithms::qsort<std::vector<int>, cmp, cmp_2>(0, 16, my_vec); //works without third template argument
 	for(int i = 0; i < my_vec.size(); ++i){
 		std::cout << my_vec[i] << " ";
 	}
@@ -64,13 +78,10 @@ int main(){
 	std::cout << std::endl;
 	std::cout << "Radix sort" << std::endl;
 	my_vec = {9, 120, 10, 80, 112, 331, 6, 4, 25, 5, 13, 7};
-	data_structures::radix_sort(3, 12, my_vec);
+	sorting_algorithms::radix_sort(3, 12, my_vec);
 	for(int i = 0; i < my_vec.size(); ++i){
 		std::cout << my_vec[i] << " ";
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
 }
-
-
-
---
+++ sorting_algorithms.hpp
@@ -0,0 +1,179 @@
+#ifndef __SORTING_ALGORITHMS_HPP__
+#define __SORTING_ALGORITHMS_HPP__
+
+#include<algorithm>
+#include<iostream>
+#include<vector>
+#include<list>
+#include<math.h>
+
+namespace sorting_algorithms{
+	struct cmp{
+		template<typename T, typename Q>
+		bool operator()(T lhs, Q rhs){
+			return lhs > rhs;
+		}
+	};
+
+	struct cmp_2{
+		template<typename T, typename Q>
+		bool operator()(T lhs, Q rhs){
+			return lhs < rhs;
+		}
+	};
+
+	template<typename T, typename Comp = cmp>
+	void bsort(T& array){   			
+		Comp cmp; 	
+		for(size_t i = 0; i < array.size(); ++i){                   
+			for(size_t j = 0; j < array.size() - 1; ++j){
+				if(cmp(array[j + 1], array[j])){
+					std::swap(array[j], array[j + 1]);
+				}
+			}
+		}
+	}
+
+	//Sometimes this sort algorithm doesn't work properly. TODO fix.
+	template<typename T, typename Comp = cmp>
+	void shake_sort(T& array){
+		Comp cmp;
+		size_t leftMark = 0;
+		size_t rightMark = array.size() - 1;                        
+		while(leftMark < rightMark){
+			for(size_t i = leftMark; i <= rightMark; ++i){
+				if(cmp(array[i + 1], array[i])){
+					std::swap(array[i], array[i + 1]);
+				}
+			}
+			rightMark--;
+
+			for(size_t i = rightMark; i > leftMark; --i){
+				if(cmp(array[i], array[i - 1])){
+					std::swap(array[i], array[i - 1]);
+				}
+			}
+			leftMark++;
+		}
+	}
+
+	template<typename T, typename Comp = cmp>
+	void comb_sort(T& array){  
+		Comp cmp;                                   
+		size_t n = array.size() - 1;
+		size_t s = n;
+		size_t k;
+		size_t tmp;
+		while(n > 1){
+			// 1.247 is a specific constant for comb sort
+			s /= 1.247f;
+			if(s < 1) s = 1;
+			k = 0;
+			for(size_t i = 0; i + s <= n; ++i){
+				if(cmp(array[i + s], array[i])){
+					tmp = array[i];
+					array[i] = array[i + s];
+					array[i + s] = tmp;
+					k = i;
+				}
+			}
+			if(s == 1) n = k + 1;
+		}
+	}
+
+	template<typename T, typename Comp = cmp>
+	void insertion_sort(T& array){
+		Comp cmp;
+		for(size_t i = 1; i < array.size(); ++i){                     
+			for(size_t j = i; j > 0 && cmp(array[j], array[j - 1]); j--){
+				std::swap(array[j - 1], array[j]);
+			}
+		}
+	}
+
+	template<typename T, typename Comp = cmp>
+	void selection_sort(T& array){   //
+		Comp cmp;                               
+		size_t min = 0;
+		size_t max = array.size();
+		while(min < max){
+			for(size_t i = min + 1; i < max; i++){
+				if(cmp(array[i], array[min])){
+					std::swap(array[i], array[min]);
+				}
+			}
+			min++;
+		}
+	}
+
+	template<typename T, typename Comp = cmp>
+	void double_selection_sort(T& array){  
+		Comp cmp;                        
+		size_t min = 0;
+		size_t max = array.size();
+		while(min < max){
+			for(size_t i = min + 1; i < max; i++){
+				if(cmp(array[i], array[min])){
+					std::swap(array[i], array[min]);
+				}
+			}
+			for(size_t i = max - 2; i > min; i--){
+				if(cmp(array[max - 1], array[i])){
+					std::swap(array[i], array[max - 1]);
+				}
+			}
+			min++;
+			max--;
+		}
+	}
+
+	template<typename T, typename Comp = cmp, typename Comp_2 = cmp_2>
+	void qsort(size_t first, size_t last, T& array){ 
+		Comp cmp;
+		Comp_2 cmp_2;        
+		if(first < last){
+			auto mid = array[(first + last) / 2]; 
+			int i = first;
+			int j = last;
+			while(i <= j){
+				while(cmp(array[i], mid)) i++;
+				while(cmp_2(array[j], mid)) j--;
+				if(i <= j) std::swap(array[i++], array[j--]);
+			}
+			if(j > 0) qsort(first, j, array);
+			if(i < last) qsort(i, last, array);
+		}	
+	}
+
+	template<typename T>
+	void radix_sort(size_t max, size_t size,  std::vector<T>& arr){      
+		size_t i, j, m;
+		size_t p = 1; 
+		size_t index, temp;
+		size_t count = 0;
+		std::vector<std::vector<T>>pocket(10);
+		for(size_t i = 0; i < max + 1; i++){
+			m = pow(10, i + 1);
+			p = pow(10, i);
+			for(size_t j = 0; j < size; j++){
+				temp = arr[j] % m;
+				index = temp / p;
+				pocket[index].push_back(arr[j]);
+			}
+			count = 0;
+			for(size_t j = 0; j < 10; j++){
+				while(!pocket[j].empty()){
+					arr[count] = *(pocket[j].begin());
+					pocket[j].erase(pocket[j].begin());
+					count++;
+				}
+			}
+		}
+	}
+
+
+
+} // sorting_algorithms
+
+
+#endif // __SORTING_ALGORITHMS_HPP__
